ecd79d9a840d8708bebbcef5972ba9d62bd36137
Minji Choi <mjchoi@scgs.co.kr>
2019-10-28 10:40:23 +0900

    First commit

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require('express')
 var app = express()
+const axios = require('axios');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
+})
+
+app.get('/getweather', function(request, responsefromWeb) {
+  axios.get('https://api.weather.gov/alerts?active=1&state=MN')
+  .then(function (response) {
+    responsefromWeb.send(response.data.features);
+  })
+  .catch(function (error) {
+    console.log(error);
+    responsefromWeb.send(error);
+  });
 })
 
 app.listen(app.get('port'), function() {
